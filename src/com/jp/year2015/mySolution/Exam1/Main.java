c853527725bd1e3f7ec4ef310caca7e8e19576c3
iqbal2907 <iqbal2907@gmail.com>
2015-10-08 20:18:17 +0530

    Added code for creating adjacency Map

--- src/com/jp/year2015/mySolution/Exam1/Main.java
+++ src/com/jp/year2015/mySolution/Exam1/Main.java
@@ -10,6 +10,8 @@ import java.util.Map;
 public class Main {
 
 	public static void main(String[] args) {
+		long time = System.currentTimeMillis();
+
 		int rows = Integer.parseInt(args[0]);
 		int columns = Integer.parseInt(args[1]);
 
@@ -82,96 +84,108 @@ public class Main {
 
 		// adding first row
 		for (int j = 0; j < columns - 1; j++) {
-			Cell node = new Cell(0, j, arr[0][j]);
-			List<Cell> list = new ArrayList<Cell>(3);
-			// up
-			if (arr[rows - 1][j] != -1) {
-				Cell nd = new Cell(rows - 1, j, arr[rows - 1][j]);
-				list.add(objMap.get(nd.hashCode()));
+			if (arr[0][j] != -1) {
+				Cell node = new Cell(0, j, arr[0][j]);
+				List<Cell> list = new ArrayList<Cell>(3);
+				// up
+				if (arr[rows - 1][j] != -1) {
+					Cell nd = new Cell(rows - 1, j, arr[rows - 1][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				// front
+				if (arr[0][j + 1] != -1) {
+					Cell nd = new Cell(0, j + 1, arr[0][j + 1]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				// down
+				if (arr[1][j] != -1) {
+					Cell nd = new Cell(1, j, arr[1][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				adjMap.put(node.hashCode(), list);
 			}
-			// front
-			if (arr[0][j + 1] != -1) {
-				Cell nd = new Cell(0, j + 1, arr[0][j + 1]);
-				list.add(objMap.get(nd.hashCode()));
-			}
-			// down
-			if (arr[1][j] != -1) {
-				Cell nd = new Cell(1, j, arr[1][j]);
-				list.add(objMap.get(nd.hashCode()));
-			}
-			adjMap.put(node.hashCode(), list);
 		}
 
 		// adding last row
 		for (int i = rows - 1, j = 0; j < columns - 1; j++) {
-			Cell node = new Cell(i, j, arr[i][j]);
-			List<Cell> list = new ArrayList<Cell>(3);
-			// up
-			if (arr[i - 1][j] != -1) {
-				Cell nd = new Cell(i - 1, j, arr[i - 1][j]);
-				list.add(objMap.get(nd.hashCode()));
+			if (arr[i][j] != -1) {
+				Cell node = new Cell(i, j, arr[i][j]);
+				List<Cell> list = new ArrayList<Cell>(3);
+				// up
+				if (arr[i - 1][j] != -1) {
+					Cell nd = new Cell(i - 1, j, arr[i - 1][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				// front
+				if (arr[i][j + 1] != -1) {
+					Cell nd = new Cell(i, j + 1, arr[i][j + 1]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				// down
+				if (arr[0][j] != -1) {
+					Cell nd = new Cell(0, j, arr[0][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				adjMap.put(node.hashCode(), list);
 			}
-			// front
-			if (arr[i][j + 1] != -1) {
-				Cell nd = new Cell(i, j + 1, arr[i][j + 1]);
-				list.add(objMap.get(nd.hashCode()));
-			}
-			// down
-			if (arr[0][j] != -1) {
-				Cell nd = new Cell(0, j, arr[0][j]);
-				list.add(objMap.get(nd.hashCode()));
-			}
-			adjMap.put(node.hashCode(), list);
 		}
 
 		// adding last column
 		for (int i = 1, j = columns - 1; i < rows - 1; i++) {
-			Cell node = new Cell(i, j, arr[i][j]);
+			if (arr[i][j] != -1) {
+				Cell node = new Cell(i, j, arr[i][j]);
+				List<Cell> list = new ArrayList<Cell>(2);
+				// up
+				if (arr[i - 1][j] != -1) {
+					Cell nd = new Cell(i - 1, j, arr[i - 1][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				// down
+				if (arr[i + 1][j] != -1) {
+					Cell nd = new Cell(i + 1, j, arr[i + 1][j]);
+					list.add(objMap.get(nd.hashCode()));
+				}
+				adjMap.put(node.hashCode(), list);
+			}
+		}
+		// adding right top corner
+		if (arr[0][columns - 1] != -1) {
+			Cell node = new Cell(0, columns - 1, arr[0][columns - 1]);
 			List<Cell> list = new ArrayList<Cell>(2);
 			// up
-			if (arr[i - 1][j] != -1) {
-				Cell nd = new Cell(i - 1, j, arr[i - 1][j]);
+			if (arr[rows - 1][columns - 1] != -1) {
+				Cell nd = new Cell(rows - 1, columns - 1,
+						arr[rows - 1][columns - 1]);
 				list.add(objMap.get(nd.hashCode()));
 			}
 			// down
-			if (arr[i + 1][j] != -1) {
-				Cell nd = new Cell(i + 1, j, arr[i + 1][j]);
+			if (arr[1][columns - 1] != -1) {
+				Cell nd = new Cell(1, columns - 1, arr[1][columns - 1]);
 				list.add(objMap.get(nd.hashCode()));
 			}
 			adjMap.put(node.hashCode(), list);
 		}
-
-		//adding right top corner
-		Cell node = new Cell(0, columns-1, arr[0][columns-1]);
-		List<Cell> list = new ArrayList<Cell>(2);
-		//up
-		if (arr[rows-1][columns-1] != -1) {
-			Cell nd = new Cell(rows - 1, columns-1, arr[rows-1][columns-1]);
-			list.add(objMap.get(nd.hashCode()));
+		// adding right bottom corner
+		if (arr[rows - 1][columns - 1] != -1) {
+			Cell node = new Cell(rows - 1, columns - 1,
+					arr[rows - 1][columns - 1]);
+			List<Cell> list = new ArrayList<Cell>(2);
+			// up
+			if (arr[rows - 2][columns - 1] != -1) {
+				Cell nd = new Cell(rows - 2, columns - 1,
+						arr[rows - 2][columns - 1]);
+				list.add(objMap.get(nd.hashCode()));
+			}
+			// down
+			if (arr[0][columns - 1] != -1) {
+				Cell nd = new Cell(0, columns - 1, arr[0][columns - 1]);
+				list.add(objMap.get(nd.hashCode()));
+			}
+			adjMap.put(node.hashCode(), list);
 		}
-		//down
-		if (arr[1][columns-1] != -1) {
-			Cell nd = new Cell(1, columns-1, arr[1][columns-1]);
-			list.add(objMap.get(nd.hashCode()));
-		}
-		adjMap.put(node.hashCode(), list);
-		
-		//adding right bottom corner
-		node = new Cell(rows-1, columns-1, arr[rows-1][columns-1]);
-		list = new ArrayList<Cell>(2);
-		//up
-		if (arr[rows-2][columns-1] != -1) {
-			Cell nd = new Cell(rows - 2, columns-1, arr[rows-2][columns-1]);
-			list.add(objMap.get(nd.hashCode()));
-		}
-		//down
-		if (arr[0][columns-1] != -1) {
-			Cell nd = new Cell(0, columns-1, arr[0][columns-1]);
-			list.add(objMap.get(nd.hashCode()));
-		}
-		adjMap.put(node.hashCode(), list);
-
 		System.out.println("\n\nAdjacency Map : \n" + adjMap);
+		System.out.println("Execution time : "
+				+ (System.currentTimeMillis() - time));
 
 	}
 }
