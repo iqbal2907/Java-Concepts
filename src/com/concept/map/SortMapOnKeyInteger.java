320cfe56bac62da7721412e7500e0c37edf7d421
Ashrafi Iqbal <ashrafii@opentext.com>
2018-04-02 11:14:35 +0530

    reverted

--- src/com/concept/map/SortMapOnKeyInteger.java
+++ src/com/concept/map/SortMapOnKeyInteger.java
@@ -10,16 +10,32 @@ import javax.swing.text.AbstractDocument.BranchElement;
 public class SortMapOnKeyInteger {
 
 	public static void main(String[] args) {
-		int i = 0;
-		for (; ;) {
-			for (i = 0; i <=5; i++) {
-				System.out.println(i);
+		//When key is Integer
+		Map<Integer, String> integerMap = new HashMap<Integer, String>();
+		integerMap.put(9, "Nine");
+		integerMap.put(7, "Seven");
+		integerMap.put(1, "One");
+		integerMap.put(3, "Three");
+		integerMap.put(2, "Two");
+		integerMap.put(5, "Five");
+		integerMap.put(8, "Eight");
+		integerMap.put(4, "Four");
+		integerMap.put(6, "Six");
+
+		Map<Integer, String> sortedIntegerMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+//				return o2-o1;
+				return o2.compareTo(o1);
 			}
-			if (i>5) {
-				break;
-			}
-		}
-	}
+		});
+		
+		sortedIntegerMap.putAll(integerMap);
+
+		System.out.println("\nMap before sorting : ");
+		printIntegerMap(integerMap); // This line may print in sorted order but its not guranteed
+		System.out.println("\nMap after sorting : ");
+		printIntegerMap(sortedIntegerMap);}
 
 
 	private static void printIntegerMap(Map<Integer, String> sortedIntegerMap) {
