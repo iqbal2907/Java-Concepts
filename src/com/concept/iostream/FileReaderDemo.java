d0dec7208520306e52a877413eccac1c2f917e1e
Ashrafi Iqbal <ashrafii@opentext.com>
2018-06-20 20:15:29 +0530

    added code to read file from package corresponding to java class also wrote code for printing file content diagonally

--- src/com/concept/iostream/FileReaderDemo.java
+++ src/com/concept/iostream/FileReaderDemo.java
@@ -11,12 +11,33 @@ import java.io.IOException;
 public class FileReaderDemo {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		FileReader fr = new FileReader("/home/iqbal/practice/workspace/JavaConcepts/io_resource/filereader.txt");
+		
+		String url = FileReaderDemo.class.getResource("filereader.txt").toString();
+		String str = url.split(":")[2];
+		System.out.println(str);
+		FileReader fr = new FileReader(str);
 		int data = 0;
+		int i = 0;
+		int g = 1;
+		char[] word = new char[20];
+		char c;
 		try {
 			while ((data = fr.read()) != -1) {
-				System.out.print((char)data);
+				c = (char)data;
+				if (c!=' ') {
+					word[i] = c;
+					i++;
+				}else{
+					System.out.println(new String(word).trim());
+					word = new char[20];
+					for (int j = 0; j < g; j++) {
+						System.out.print(" ");
+					}
+					i=0;
+				}
+				g++;
 			}
+			System.out.println(new String(word).trim());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
